c3930ea6ca89379bbdae39f69e3b57a0a741a8a5
1ag3.iorilope <1ag3.iorilope@Enara-p22.tolosaldealh.eus>
2023-11-29 10:27:30 +0100

    last

--- Proiektua2/src/Proiektua2/Autoak.java
+++ Proiektua2/src/Proiektua2/Autoak.java
@@ -220,6 +220,7 @@ static void erakutsiAutoak() {
 
 
 
+
 	
 
 }
--- Proiektua2/src/Proiektua2/Nagusia.java
+++ Proiektua2/src/Proiektua2/Nagusia.java
@@ -4,7 +4,7 @@
 package Proiektua2;
 
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  * 
@@ -21,11 +21,11 @@ public class Nagusia {
 		
 		
 		
-//		 String[][] Autozerrenda = new String[][]{
-//	            {"Benz", "Mercedes"},
-//	            {"X1", "BMW"},
-//	            {"Camry", "Toyota"}
-//	        };
+		 String[][] Autozerrenda = new String[][]{
+	            {"Benz", "Mercedes"},
+	            {"X1", "BMW"},
+	            {"Camry", "Toyota"}
+	        };
 		
 		System.out.println("==================================================================");
 		System.out.println("    Sartu zure aukera");
@@ -35,8 +35,58 @@ public class Nagusia {
 		System.out.println("	4-Auto kop ikusi");
 		System.out.println("==================================================================");
 		
+		int aukera;
+		do {
+			
+		
+		 aukera = sc.nextInt();
+		 if (aukera != 1 && aukera != 2 && aukera != 3 && aukera != 4) {
+			 
+			 System.out.println("Autatutako zenbakiak ez du balio, sartu berriro (1-4)");
+		}
+		
+		} while (aukera !=1 && aukera !=2  && aukera !=3  && aukera !=4);
 		
 		
+		if (aukera == 1) {
+			
+			System.out.println("Autoa gehitu aukeratu duzu");
+			System.out.println("Sartu autoaren Marka");
+			String Marka2 = sc.next();
+		
+		
+			
+		
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		else if (aukera == 2) {
+			
+			System.out.println("Autoa kendu aukeratu duzu");
+			
+		}
+		else if (aukera == 3) {
+			
+			
+			System.out.println("Autoak ikusi  aukeratu duzu");
+			
+		}
+		
+		else if (aukera == 4) {
+			
+			System.out.println("Autoak kop   aukeratu duzu");
+			
+		}
+		else {
+			System.out.println("Autatutako zenbakiak ez du balio");
+		}
 		
 		
 		
